be7001da4e98d25ed5b3e44cd1fc806ce65f3f4c
Hanson Char <hanson.char@gmail.com>
2025-01-16 18:11:29 -0800

    Rewrite DFS with a more classic implementation

--- learning-lua/algo/DFS.lua
+++ learning-lua/algo/DFS.lua
@@ -5,25 +5,27 @@ local DFS = GraphSearch:class()
 local E = {}
 
 local function _iterate(self)
-  local stack, visited = Stack:new(), {
-    [self.src_vertex] = true
-  }
-  self._visited_count = 1
-  local level = 0
-  local from = self.src_vertex
-  repeat
-    local vertex = self.graph:vertex(from)
-    level = level + 1
-    for to, weight in vertex:outgoings() do
-      stack:push{to, weight, level, from}
-    end
-    local t = (stack:pop() or E)
-    from, level = t[1], t[3]
-    if from and not visited[from] then
+  local stack, visited = Stack:new(), {}
+  self._visited_count = 0
+  local level, from = 0, self.src_vertex
+  local t
+  while from do
+    if not visited[from] then
       visited[from], self._visited_count = true, self._visited_count + 1
-      self._yield(t)
+      if t then -- t is nil only during the first iteration when we don't yet have an edge to yield
+        self._yield(t)
+      end
+      local vertex = self.graph:vertex(from)
+      level = level + 1
+      for to, weight in vertex:outgoings() do
+        if not visited[to] then
+          stack:push{to, weight, level, from}
+        end
+      end
     end
-  until not from
+    t = (stack:pop() or E)
+    from, level = t[1], t[3]
+  end
 end
 
 ---@param G (table) graph
