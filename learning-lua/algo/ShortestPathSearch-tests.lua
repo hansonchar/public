ce1455e2bb14ac28fbdf943bdd819b20ac7406d1
Hanson Char <hanson.char@gmail.com>
2025-02-01 09:37:25 -0800

    formatting

--- learning-lua/algo/ShortestPathSearch-tests.lua
+++ learning-lua/algo/ShortestPathSearch-tests.lua
@@ -39,8 +39,8 @@ end
 --- Example from Algorithms Illuminated Part 2, 2018, by Tim Roughgarden.
 local function tim_test()
   print("ShortestPathSearch tim_test...")
-  local input<const> = {'s-v=1', 's-w=4', 'v-w=2', 'v-t=6', 'w-t=3'}
-  local src<const> = 's'
+  local input <const> = {'s-v=1', 's-w=4', 'v-w=2', 'v-t=6', 'w-t=3'}
+  local src <const> = 's'
   local G = load_input(input)
   local level_counts = {}
   local search = ShortestPathSearch:new(G)
@@ -67,10 +67,10 @@ end
 -- Example from https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
 local function geek_test()
   print("ShortestPathSearch geek_test...")
-  local input<const> = {'0-1=4', '1-0=4', '0-7=8', '7-0=8', '1-2=8', '2-1=8', '1-7=11', '7-1=11', '2-3=7', '3-1=7',
-                        '2-5=4', '5-2=4', '2-8=2', '8-2=2', '3-4=9', '4-3=9', '3-5=14', '5-3=14', '4-5=10', '5-4=10',
-                        '5-6=2', '6-5=2', '6-7=1', '7-6=1', '6-8=6', '8-6=6', '7-8=7', '8-7=7'}
-  local src<const> = '0'
+  local input <const> = {'0-1=4', '1-0=4', '0-7=8', '7-0=8', '1-2=8', '2-1=8', '1-7=11', '7-1=11', '2-3=7', '3-1=7',
+    '2-5=4', '5-2=4', '2-8=2', '8-2=2', '3-4=9', '4-3=9', '3-5=14', '5-3=14', '4-5=10', '5-4=10',
+    '5-6=2', '6-5=2', '6-7=1', '7-6=1', '6-8=6', '8-6=6', '7-8=7', '8-7=7'}
+  local src <const> = '0'
   local G = load_input(input)
   local level_counts = {}
   local search = ShortestPathSearch:new(G)
@@ -117,8 +117,8 @@ end
 -- https://www.redblobgames.com/pathfinding/posts/reprioritize.html
 local function redblobgames_test()
   print("ShortestPathSearch redblobgames_test...")
-  local input<const> = {'A-B=1', 'B-C=1', 'B-E=4', 'C-D=1', 'D-E=1'}
-  local src<const> = 'A'
+  local input <const> = {'A-B=1', 'B-C=1', 'B-E=4', 'C-D=1', 'D-E=1'}
+  local src <const> = 'A'
 
   local G = load_input(input)
   local level_counts = {}
@@ -153,9 +153,9 @@ end
 -- https://algodaily.com/lessons/an-illustrated-guide-to-dijkstras-algorithm
 local function algodaily_test()
   print("ShortestPathSearch algodaily_test...")
-  local input<const> = {'A-B=5', 'A-C=2', 'B-C=7', 'B-D=8', 'C-D=4', 'C-E=8', 'D-E=6', 'D-F=4', 'E-F=3', 'B-A=5',
-                        'C-A=2', 'C-B=7', 'D-B=8', 'D-C=4', 'E-C=8', 'E-D=6', 'F-D=4', 'F-E=3'}
-  local src<const> = 'A'
+  local input <const> = {'A-B=5', 'A-C=2', 'B-C=7', 'B-D=8', 'C-D=4', 'C-E=8', 'D-E=6', 'D-F=4', 'E-F=3', 'B-A=5',
+    'C-A=2', 'C-B=7', 'D-B=8', 'D-C=4', 'E-C=8', 'E-D=6', 'F-D=4', 'F-E=3'}
+  local src <const> = 'A'
 
   local G = load_input(input)
   local level_counts = {}
@@ -193,9 +193,9 @@ end
 -- CE 191 - CEE Systems Analysis by Processor Scott Moura, University of California, Berkeley
 local function scott_moura_test()
   print("ShortestPathSearch scott_moura_test...")
-  local input<const> = {'A-B=2', 'A-C=4', 'A-D=4', 'B-F=6', 'C-F=5', 'C-E=7', 'C-D=1', 'D-E=5', 'D-H=11', 'E-G=3',
-                        'E-H=4', 'F-H=5', 'F-G=2', 'F-E=1', 'G-H=2'}
-  local src<const> = 'A'
+  local input <const> = {'A-B=2', 'A-C=4', 'A-D=4', 'B-F=6', 'C-F=5', 'C-E=7', 'C-D=1', 'D-E=5', 'D-H=11', 'E-G=3',
+    'E-H=4', 'F-H=5', 'F-G=2', 'F-E=1', 'G-H=2'}
+  local src <const> = 'A'
 
   local G = load_input(input)
   local level_counts = {}
@@ -243,9 +243,9 @@ local function negative_tests()
   G:add('s', 'v', -1)
   local search = ShortestPathSearch:new(G)
   local iterator = search:iterate('s')
-  local ok, errmsg = pcall(iterator, search)
+  local ok, err = pcall(iterator, search)
   assert(not ok)
-  assert(string.match(errmsg, "Weight must not be negative$"))
+  assert(string.match(err, "Weight must not be negative$"))
 end
 
 basic_tests()
