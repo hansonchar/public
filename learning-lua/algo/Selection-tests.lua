8edf9521bcb0e2c6f2d30b392b52231cb548959d
Hanson Char <hanson.char@gmail.com>
2025-02-01 18:42:50 -0800

    Support computing the median of medians

--- learning-lua/algo/Selection-tests.lua
+++ learning-lua/algo/Selection-tests.lua
@@ -3,57 +3,90 @@ local DEBUG = require "algo.Debug":new(false)
 local debug, debugf, concat = DEBUG.debug, DEBUG.debugf, DEBUG.concat
 local S = Selection:new()
 
-local ar = {6, 8, 9, 2}
-local val, ar_o, count = S:rselect(ar, 2)
-assert(val == 6, val)
-debugf("Resultant array: %s, swaps: %d", concat(ar_o, ","), count)
+local function rselect_tests()
+  print("Running rselect_tests ...")
+  local ar = {6, 8, 9, 2}
+  local val, ar_o, count = S:rselect(ar, 2)
+  assert(val == 6, val)
+  debugf("Resultant array: %s, swaps: %d", concat(ar_o, ","), count)
 
-local ar = {6, 8, 9, 2}
-local val, ar_o, count = S:rselect(ar, 3)
-assert(val == 8, val)
-debugf("Resultant array: %s, swaps: %d", concat(ar_o, ","), count)
+  local ar = {6, 8, 9, 2}
+  local val, ar_o, count = S:rselect(ar, 3)
+  assert(val == 8, val)
+  debugf("Resultant array: %s, swaps: %d", concat(ar_o, ","), count)
 
-local ar = {3, 8, 2, 5, 1, 4, 7, 6}
-local val, ar_o, count = S:rselect(ar, #ar >> 1)
-assert(val == 4, val)
-debugf("Resultant array: %s, swaps: %d", concat(ar_o, ","), count)
+  local ar = {3, 8, 2, 5, 1, 4, 7, 6}
+  local val, ar_o, count = S:rselect(ar, #ar >> 1)
+  assert(val == 4, val)
+  debugf("Resultant array: %s, swaps: %d", concat(ar_o, ","), count)
 
-local ar = {3, 8, 2, 5, 1, 4, 7, 6}
-local val, ar_o, count = S:rselect(ar, #ar >> 1)
-assert(val == 4, val)
-debugf("Resultant array: %s, swaps: %d", concat(ar_o, ","), count)
+  local ar = {3, 8, 2, 5, 1, 4, 7, 6}
+  local val, ar_o, count = S:rselect(ar, #ar >> 1)
+  assert(val == 4, val)
+  debugf("Resultant array: %s, swaps: %d", concat(ar_o, ","), count)
+end
 
-local ar = {5, 2, 1, 4, 3}
-local median = S.median_of_5(table.unpack(ar))
-debugf("Median of %s: %d", concat(ar, ","), median)
-assert(median == 3)
+local function median_of_5_tests()
+  print("Running median_of_5_tests ...")
+  local ar = {5, 2, 1, 4, 3}
+  local median, pos = S.median_of_5(table.unpack(ar))
+  debugf("Median of %s: %d", concat(ar, ","), median)
+  assert(median == 3 and pos == #ar)
 
-local ar = {5, 2, 1, 4}
-local median = S.median_of_5(table.unpack(ar))
-debugf("Median of %s: %d", concat(ar, ","), median)
-assert(median == 2)
+  local ar = {5, 2, 1, 4}
+  local median, pos = S.median_of_5(table.unpack(ar))
+  debugf("Median of %s: %d", concat(ar, ","), median)
+  assert(median == 2 and pos == 2)
 
-local ar = {5, 3, 1, 2}
-local median = S.median_of_5(table.unpack(ar))
-debugf("Median of %s: %d", concat(ar, ","), median)
-assert(median == 2)
+  local ar = {5, 3, 1, 2}
+  local median, pos = S.median_of_5(table.unpack(ar))
+  debugf("Median of %s: %d", concat(ar, ","), median)
+  assert(median == 2 and pos == #ar)
 
-local ar = {5, 1, 2}
-local median = S.median_of_5(table.unpack(ar))
-debugf("Median of %s: %d", concat(ar, ","), median)
-assert(median == 2)
+  local ar = {5, 1, 2}
+  local median, pos = S.median_of_5(table.unpack(ar))
+  debugf("Median of %s: %d", concat(ar, ","), median)
+  assert(median == 2 and pos == #ar)
 
-local ar = {5, 1}
-local median = S.median_of_5(table.unpack(ar))
-debugf("Median of %s: %d", concat(ar, ","), median)
-assert(median == 1)
+  local ar = {5, 1}
+  local median, pos = S.median_of_5(table.unpack(ar))
+  debugf("Median of %s: %d", concat(ar, ","), median)
+  assert(median == 1 and pos == #ar)
 
-local ok, err = pcall(S.median_of_5)
-assert(not ok)
-assert(err:match("At least one argument"))
-debug(err)
+  local ok, err = pcall(S.median_of_5)
+  assert(not ok)
+  assert(err:match("At least one argument"))
+  debug(err)
 
-local ok, err = pcall(S.median_of_5, 1, 2, 3, 4, 5, 6)
-assert(not ok)
-assert(err:match("At most five arguments"))
-debug(err)
+  local ok, err = pcall(S.median_of_5, 1, 2, 3, 4, 5, 6)
+  assert(not ok)
+  assert(err:match("At most five arguments"))
+  debug(err)
+
+  local mom = S.median_of_medians({1, 3, 6})
+  assert(mom == 3)
+
+  local ar = {3, 8, 2, 5, 1, 4, 7, 6}
+  local median = S.median_of_medians(ar)
+  assert(median == 3)
+end
+
+local function median_of_medians_tests()
+  print("Running median_of_medians_tests ...")
+  local ar = {11, 6, 10, 2, 15, 8, 1, 7, 14, 3, 9, 12, 4, 5, 13}
+  local median, pos = S.median_of_medians(ar)
+  assert(median == 9 and pos == 11)
+
+  local ar = {12, 15, 11, 2, 9, 5, 0, 7, 3, 21, 44, 40, 1, 18, 20, 32, 19, 35, 37, 39,
+    13, 16, 14, 8, 10, 26, 6, 33, 4, 27, 49, 46, 52, 25, 51, 34, 43, 56, 72, 79,
+    17, 23, 24, 28, 29, 30, 31, 36, 42, 47, 50, 55, 58, 60, 63, 65, 66, 67, 81, 83,
+    22, 45, 38, 53, 61, 41, 62, 82, 54, 48, 59, 57, 71, 78, 64, 80, 70, 76, 85, 87,
+    96, 95, 94, 86, 89, 69, 68, 97, 73, 92, 74, 88, 99, 84, 75, 90, 77, 93, 98, 91,
+  }
+  local median, pos = S.median_of_medians(ar)
+  assert(median == 36 and pos == 48)
+end
+
+rselect_tests()
+median_of_5_tests()
+median_of_medians_tests()
--- learning-lua/algo/Selection.lua
+++ learning-lua/algo/Selection.lua
@@ -2,6 +2,9 @@ local Partition = require "algo.Partition"
 local Selection = {}
 Selection.__index = Selection
 
+local DEBUG = require "algo.Debug":new(false)
+local debug, debugf, concat = DEBUG.debug, DEBUG.debugf, DEBUG.concat
+
 ---Returns the median of the given five or less number of values.
 ---@param a any
 ---@param b any?
@@ -9,6 +12,7 @@ Selection.__index = Selection
 ---@param d any?
 ---@param e any?
 ---@return any median the median value.
+---@return any pos position of the median value in the input.
 function Selection.median_of_5(a, b, c, d, e, ...)
   assert(not ..., "At most five arguments")
   if not b then
@@ -16,7 +20,11 @@ function Selection.median_of_5(a, b, c, d, e, ...)
     return assert(a, "At least one argument")
   elseif not c then
     assert(not d and not e)
-    return a < b and a or b
+    if a < b then
+      return a, 1
+    else
+      return b, 2
+    end
   elseif not d then
     assert(not e)
     d = d or -math.huge
@@ -25,16 +33,53 @@ function Selection.median_of_5(a, b, c, d, e, ...)
     e = e or -math.huge
   end
   local min, min2, min3 = math.huge, math.huge, math.huge
-  for _, val in pairs {a, b, c, d, e} do
+  local min_pos, min2_pos, min3_pos
+  for pos, val in pairs {a, b, c, d, e} do
     if val < min then
       min3, min2, min = min2, min, val
+      min3_pos, min2_pos, min_pos = min2_pos, min_pos, pos
     elseif val < min2 then
       min3, min2 = min2, val
+      min3_pos, min2_pos = min2_pos, pos
     elseif val < min3 then
-      min3 = val
+      min3, min3_pos = val, pos
     end
   end
-  return min3
+  return min3, min3_pos
+end
+
+-- local function min(a, b)
+--   return a < b and a or b
+-- end
+
+---Returns the median of the medians of group of at most 5 elements, recursively.
+---@param ar (table) array of elements.
+---@return (any) median the median of the medians of group of at most 5 elements, recursively.
+---@return (number) position the position of the median of the medians in 'ar'.
+function Selection.median_of_medians(ar)
+  assert(#ar > 0)
+  local ar_pos = {}
+  -- array of medians, and their respective positions in the original input array.
+  local medians, medians_pos = {}, {}
+  repeat
+    local groups_of_5 = math.ceil(#ar / 5)
+    local start_pos, end_pos = 1, 5
+    for _ = 1, groups_of_5 do
+      end_pos = end_pos < #ar and end_pos or #ar
+      local median, pos_1to5 = Selection.median_of_5(table.unpack(ar, start_pos, end_pos))
+      medians[#medians + 1] = median
+      local pos = pos_1to5 + start_pos - 1 -- from relative to absolute position in 'ar'.
+      -- 'median_pos' is the position of 'median' in the original input array.
+      local median_pos = ar_pos[pos] or pos -- the 'or' is applicable only during the first 'repeat' loop
+      medians_pos[#medians_pos + 1] = median_pos
+      debugf("median:%d of %s", medians[#medians], concat(ar, ",", start_pos, end_pos))
+      start_pos, end_pos = start_pos + 5, end_pos + 5
+    end
+    debug()
+    ar, ar_pos = medians, medians_pos
+    medians, medians_pos = {}, {}
+  until #ar == 1
+  return ar[1], ar_pos[1]
 end
 
 ---Select the kth smallest/greatest element of the given array via random pivot selection.
